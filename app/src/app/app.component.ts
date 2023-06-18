b90bab4f00065a9fb3ba4a3617e76c8eef177331
JGedff <113793787+JGedff@users.noreply.github.com>
2023-06-18 21:44:09 +0200

    Add files via upload

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Game } from 'src/model/Game';
+import { Genre } from 'src/model/Genre';
+import { Pegi } from 'src/model/Pegi';
+import { Platform } from 'src/model/Platform';
 import { Post } from 'src/model/Post';
+import { User } from 'src/model/User';
 
 import { FirebaseService } from 'src/services/firebase/firebase.service';
 
@@ -9,27 +14,93 @@ import { FirebaseService } from 'src/services/firebase/firebase.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  page: Number = 1;
+  page: number = 1;
+
+  user: any;
 
   arrayPosts: Post[] = [];
   arrayGames: Game[] = [];
+  arrayPegis: Pegi[] = [];
+  arrayPlatforms: Platform[] = [];
+  arrayGenres: Genre[] = [];
 
-  constructor(private _firebaseService: FirebaseService) {
+  ngOnInit(): void {
+    const userInformation = sessionStorage.getItem('app.user');
+    let page = Number(sessionStorage.getItem('app.page'))
+    
+    if (page) {
+      this.changePage(page);
+    }
+
+    if(userInformation) {
+      let oneUser = false;
+      let userId = JSON.parse(userInformation);
+      let userInfo: User;
+
+      this.user = this._firebaseService.getUserId(userId)
+        .then(array => {
+          array.forEach((element: any) => {
+            if (!oneUser) {
+              oneUser = true;
+
+              userInfo = {
+                userId: element.data().userId,
+                email: element.data().email,
+                userName: element.data().userName,
+                password: element.data().password,
+                totalLikes: element.data().totalLikes,
+                totalDislikes: element.data().totalDislikes,
+                totalPosts: element.data().totalPosts,
+                totalComments: element.data().totalComments,
+                totalRevews: element.data().totalRevews,
+                admin: element.data().admin,
+              }
+            }
+          });
+        })
+        .finally(() => {
+          this.user = userInfo;
+        })
+    }
+  }
+
+  constructor(private _firebaseService: FirebaseService, private _toastrService: ToastrService) {
 
   }
 
   changePage(pages: any) {
     this.page = pages;
-  
+
+    sessionStorage.setItem('app.page', this.page.toString());
+
+    if (this.page == 2 && this.arrayGames.length <= 0) {
+      this.loadGamesInfo();
+    }
+
     if (this.page == 3 && this.arrayPosts.length <= 0) {
       this.loadPostsInfo();
     }
+  }
 
-    else if (this.page == 2 && this.arrayGames.length <= 0) {
-      this.loadGamesInfo();
-    }
+  loadPosts(input: any): void {
+    this.arrayPosts = [];
+
+    this._firebaseService.getPosts()
+      .then(array => {
+
+        array.forEach((element: any) => {
+
+          this.arrayPosts.push({
+            idPost: element.id,
+            ...element.data()
+          });
+        });
+      })
+      .catch(error => {
+        this._toastrService.error('There was an error loading the posts:\n' + error.message, 'Posts error: ' + error.code);
+      });
   }
 
   loadPostsInfo(): void {
@@ -43,12 +114,30 @@ export class AppComponent {
             idPost: element.id,
             ...element.data()
           });
-        })
+        });
+      })
+      .catch(error => {
+        this._toastrService.error('There was an error loading the posts:\n' + error.message, 'Posts error: ' + error.code);
       });
   }
 
+  loadGames(input: any): void {
+    this.loadGamesInfo();
+  }
+
   loadGamesInfo(): void {
-    
+    this.arrayGames = [];
+    this.arrayPegis = [];
+    this.arrayGenres = [];
+    this.arrayPlatforms = [];
+
+    this.loadGamesObjects();
+    this.loadPegis();
+    this.loadPlatforms();
+    this.loadGenres();
+  }
+
+  loadGamesObjects() {
     this._firebaseService.getGames()
       .then(array => {
 
@@ -58,11 +147,70 @@ export class AppComponent {
             barCode: element.id,
             ...element.data()
           });
-        })
+        });
+      })
+      .catch(error => {
+        this._toastrService.error('There was an error loading the games:\n' + error.message, 'Game error: ' + error.code);
+      });
+  }
+
+  loadPegis() {
+    this._firebaseService.getPegis()
+      .then(array => {
+        array.forEach((doc: any) => {
+          let PEGI: Pegi = {
+            name: doc.data().name
+          }
+          
+          this.arrayPegis.push(PEGI);
+        });
+      })
+      .catch(error => {
+        this._toastrService.error("There was an error loading:\n" + error.message, 'Error loading pegi: ' + error.code);
+      });
+  }
+
+  loadPlatforms() {
+    this._firebaseService.getPlatforms()
+      .then(array => {
+        array.forEach((doc: any) => {
+          let PLATFORM: Platform = {
+            name: doc.data().name
+          }
+
+          this.arrayPlatforms.push(PLATFORM);
+        });
+      })
+      .catch(error => {
+        this._toastrService.error("There was an error loading:\n" + error.message, 'Error loading platform: ' + error.code);
+      });
+  }
+
+  loadGenres() {
+    this._firebaseService.getGenres()
+      .then(array => {
+        array.forEach((doc: any) => {
+          let GENRE: Genre = {
+            name: doc.data().name
+          }
+
+          this.arrayGenres.push(GENRE);
+        });
+      })
+      .catch(error => {
+        this._toastrService.error("There was an error loading:\n" + error.message, 'Error loading genre: ' + error.code);
       });
   }
 
   loadNewPost(post: any) {
     this.arrayPosts.push(post);
+  }
+
+  loadNewGame(game: any) {
+    this.arrayGames.push(game);
+  }
+
+  changeUser(user: any) {
+    this.user = user;
   }
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+
+//Firebase imports
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+//Toastr imports
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
+
+import { enviroment } from 'src/enviroments/enviroment';
 
 //Used components
 import { PostComponent } from 'src/app/components/frontend/post/post.component';
@@ -16,15 +26,18 @@ import { HomeComponent } from 'src/app/components/frontend/home/home.component';
 import { GamesComponent } from 'src/app/components/frontend/games/games.component';
 import { RevewsComponent } from 'src/app/components/frontend/revews/revews.component';
 import { ContactComponent } from 'src/app/components/frontend/contact/contact.component';
-import { AddPostComponent } from 'src/app/components/add/add-post/add-post.component';
-import { AddCommentComponent } from 'src/app/components/add/add-comment/add-comment.component';
-import { AddRevewComponent } from 'src/app/components/add/add-revew/add-revew.component';
-
-//Firebase imports
-import { provideFirebaseApp,  initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-
-import { enviroment } from 'src/enviroments/enviroment';
+import { AddPostComponent } from 'src/app/components/add-edit/add-post/add-post.component';
+import { AddCommentComponent } from 'src/app/components/add-edit/add-comment/add-comment.component';
+import { AddRevewComponent } from 'src/app/components/add-edit/add-revew/add-revew.component';
+import { LoginComponent } from './components/frontend/login/login.component';
+import { UserInfoComponent } from './components/frontend/user-info/user-info.component';
+import { AddGameComponent } from './components/add-edit/add-game/add-game.component';
+import { DashboardComponent } from './components/administration/dashboard/dashboard.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { EditPostComponent } from './components/add-edit/edit-post/edit-post.component';
+import { EditCommentComponent } from './components/add-edit/edit-comment/edit-comment.component';
+import { EditRevewComponent } from './components/add-edit/edit-revew/edit-revew.component';
+import { EditGameComponent } from './components/add-edit/edit-game/edit-game.component';
 
 @NgModule({
   declarations: [
@@ -39,7 +52,16 @@ import { enviroment } from 'src/enviroments/enviroment';
     ContactComponent,
     AddPostComponent,
     AddCommentComponent,
-    AddRevewComponent
+    AddRevewComponent,
+    LoginComponent,
+    UserInfoComponent,
+    AddGameComponent,
+    DashboardComponent,
+    CalculatorComponent,
+    EditPostComponent,
+    EditCommentComponent,
+    EditRevewComponent,
+    EditGameComponent
   ],
   imports: [
     BrowserModule,
@@ -47,6 +69,9 @@ import { enviroment } from 'src/enviroments/enviroment';
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(enviroment.firebase)),
     provideFirestore(() => getFirestore()),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
